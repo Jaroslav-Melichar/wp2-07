7a9daea1e106e94dfa7f1c29b62998d39033ea3d
Jaroslav-Melichar <79139968+Jaroslav-Melichar@users.noreply.github.com>
2021-11-26 13:37:50 +0100

    Create functions2.php

---
+++ functions2.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function myDummyFunction(int$age){
+    if ($age>= 18){
+        echo "jsi dospělý";
+    }else{
+        echo"nejsi dospělý ještě musíš počkat";
+    }
+    echo "<br>";
+}
+
+?>
